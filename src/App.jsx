7e891567a2e15727928a6b39b95011e0d86b6d63
Trinity Ndlovu <ndlovutrinity18@gmail.com>
2026-02-19 23:20:30 +0200

    Structured the project with HTMLand installed bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 import axios from "axios"
+import Weather from './Weather.jsx'
+
 export default function App() {
   return(
     <div className="form">
-      <h1>Weather App</h1>
-    <form>
-    <input type="text" placeholder="Type a city..." className="text-input" />
-    <input type="submit" value="search" className="send" />
-    </form>
+      <div className="container">
+      <Weather/>
+       <footer>
+                This project was coded by <a href="https://stalwart-frangollo-13a334.netlify.app/" target="_blank" rel="noreferrer noopener"> Trinity Ndlovu </a> 
+                and <a href="https://github.com/ndlovutrinity18-a11y/React-Weather-App" target="_blank" rel="noreferrer noopener"> is open-sourced on GitHub </a> and 
+                hosted on <a href="https://teereact-weather-app.netlify.app/" target="_blank" rel="noreferrer noopener"> Netlify</a>.
+            </footer>
+    </div>
     </div>
   );
   
--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,17 +1,83 @@
 import React from "react";
-export default function Weather(){
-    return(
-        <div className="main">
-<h4>
-    Cape Town <br />
-    🌧 12°C
-</h4>
-<p>
-Tuesday 20:00, light rain
-</p>
-<p>
-    Humidity:20%, Wind 30km/h
-</p>
+import "./Weather.css";
+
+export default function Weather() {
+    return (
+        <div className="weather">
+            <form>
+                <input type="text" placeholder="Type a city..." className="text-input" />
+                <input type="submit" value="search" className="send" />
+        
+            </form>
+            <h4>
+                Cape Town <br />
+                🌧 12°C
+            </h4>
+            <div className="row">
+                <div className="col-6">
+            <ul>
+            <li>
+                Tuesday 20:00, light rain
+            </li>
+            <li>
+                Humidity:20%, Wind 30km/h
+            </li>
+            </ul>
+            </div>
+            <div className="col-6">
+            <ul>
+                <li>Precipitation: 10%</li>
+                <li>Humidity: 60%</li>
+                <li>Wind: 30km/h</li>
+            </ul>
+                </div>
+            </div>
+            <h4>Hourly Forecast</h4>
+            <div className="row">
+                <div className="col-2">
+                    <p>20:00<br />🌧 12°C</p>
+                </div>
+                <div className="col-2">
+                    <p>21:00<br />🌧 11°C</p>
+                    </div>
+                <div className="col-2">
+                    <p>22:00<br />🌧 10°C</p>
+                </div>
+                <div className="col-2">
+                    <p>22:00<br />🌧 10°C</p>
+                </div>
+                <div className="col-2">
+                    <p>22:00<br />🌧 10°C</p>
+                </div>
+                <div className="col-2">
+                    <p>22:00<br />🌧 10°C</p>
+                </div>
+        </div>
+        <h4>7-Day Forecast</h4>
+        <div className="row">
+            <div className="col-2">
+                <p>Today<br />🌧 10°C</p>
+                </div>
+            <div className="col-2">
+                <p>Fri<br />🌧 12°C</p>
+            </div>
+            <div className="col-2">
+                <p>Sat<br />🌧 11°C</p>
+                </div>
+                <div className="col-2">
+                <p>Sun<br />🌧 10°C</p>
+                </div>
+                <div className="col-2">
+                <p>Mon<br />🌧 10°C</p>
+                </div>
+                <div className="col-2">
+                <p>Tue<br />🌧 10°C</p>
+                </div>
+                <div className="col-2">
+                <p>Wed<br />🌧 10°C</p>
+                </div>
+                
+            </div>
         </div>
     );
 }
